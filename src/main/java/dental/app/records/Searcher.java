6b8e06ab67a8661926c24be7f12ebfc702589285
Stanislav Kuprienko <stasi.architekk@gmail.com>
2023-08-04 01:52:25 +0600

    stuff Searcher.java

--- src/main/java/dental/app/records/Searcher.java
+++ src/main/java/dental/app/records/Searcher.java
@@ -1,7 +1,34 @@
 package dental.app.records;
 
+import java.util.ArrayList;
+
 public class Searcher {
 
+    private final RecordManager recordManager;
+    ArrayList<Record> relevant;
 
+    public Searcher(RecordManager recordManager) {
+        this.recordManager = recordManager;
+        this.relevant = new ArrayList<>();
+    }
+
+    //TODO
+
+    public void search(String patient) {
+        for (Record r : recordManager.getRecords()) {
+            if (r.getPatient().equalsIgnoreCase(patient)) {
+                relevant.add(r);
+            }
+        }
+    }
+
+    public void search(String patient, String clinic) {
+        for (Record r : recordManager.getRecords()) {
+            if ((r.getPatient().equalsIgnoreCase(patient))
+                    && (r.getClinic().equalsIgnoreCase(clinic))) {
+                relevant.add(r);
+            }
+        }
+    }
 
 }
