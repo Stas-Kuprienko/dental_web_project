5f584e6624506638bbaaa3e9bdf4b413f3cc95c1
Stanislav Kuprienko <stasi.architekk@gmail.com>
2023-09-11 01:28:44 +0600

    update WorkRecordDAO.java

--- src/main/java/dental/database/dao/WorkRecordDAO.java
+++ src/main/java/dental/database/dao/WorkRecordDAO.java
@@ -1,17 +1,16 @@
 package dental.database.dao;
 
 import dental.app.MyList;
+import dental.app.works.Product;
 import dental.app.works.WorkRecord;
 import dental.database.DBConfig;
-import org.apache.tomcat.jakartaee.commons.io.IOUtils;
 
-import javax.sql.rowset.serial.SerialBlob;
-import java.io.FileInputStream;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.OutputStream;
+import java.lang.reflect.Constructor;
+import java.sql.*;
 
 public class WorkRecordDAO implements DAO<WorkRecord>{
 
@@ -21,6 +20,7 @@ public class WorkRecordDAO implements DAO<WorkRecord>{
 
     private final int accountId;
     public static final String TABLE_NAME = "work_record";
+    private static final String PHOTO_FORMAT = "jpg";
 
     @Override
     public void add(WorkRecord workRecord) throws SQLException, NoSuchFieldException, IllegalAccessException {
@@ -36,9 +36,10 @@ public class WorkRecordDAO implements DAO<WorkRecord>{
         statement.setDate(5, Date.valueOf(workRecord.getAccepted()));
         statement.setBoolean(6, workRecord.isClosed());
         if (workRecord.getPhoto() != null) {
-            try (FileInputStream fileIn = new FileInputStream(workRecord.getPhoto())) {
-                byte[] photoBytes = IOUtils.toByteArray(fileIn);
-                statement.setBlob(7, new SerialBlob(photoBytes));
+            Blob blob = request.createBlob();
+            try (OutputStream out = blob.setBinaryStream(1)) {
+                ImageIO.write(workRecord.getPhoto(), PHOTO_FORMAT, out);
+                statement.setBlob(7, blob);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -49,20 +50,34 @@ public class WorkRecordDAO implements DAO<WorkRecord>{
         statement.executeUpdate();
         DBRequest.setID(workRecord, statement);
         request.close();
-        if (!(workRecord.getProducts().isEmpty())) {
+        MyList<Product> products = workRecord.getProducts();
+        if (!(products.isEmpty())) {
             ProductDAO productDAO = new ProductDAO(workRecord.getId());
-            productDAO.add(workRecord.getProducts().get(0));
+            for (Product p : products) {
+                productDAO.add(p);
+            }
         }
     }
 
     @Override
     public MyList<WorkRecord> getAll() throws Exception {
-        return null;
+        String query = String.format("SELECT * FROM %s.%s;", DBConfig.DATA_BASE, TABLE_NAME);
+        DBRequest request = new DBRequest(query);
+        ResultSet resultSet = request.getStatement().executeQuery();
+        MyList<WorkRecord> workRecords = new WorkRecordInstantiation(resultSet).workRecords;
+        request.close();
+        return workRecords;
     }
 
     @Override
     public WorkRecord get(int id) throws Exception {
-        return null;
+        String query = String.format("SELECT * FROM %s.%s WHERE id = ?", DBConfig.DATA_BASE, TABLE_NAME);
+        DBRequest request = new DBRequest(query);
+        request.getStatement().setInt(1, id);
+        ResultSet resultSet = request.getStatement().executeQuery();
+        WorkRecord workRecord = new WorkRecordInstantiation(resultSet).workRecords.get(0);
+        request.close();
+        return workRecord;
     }
 
     @Override
@@ -75,15 +90,47 @@ public class WorkRecordDAO implements DAO<WorkRecord>{
 
     }
 
-    public int getAccountId() {
-        return accountId;
-    }
-
+    @SuppressWarnings("all")
     private class WorkRecordInstantiation extends Instantiation<WorkRecord> {
 
+        private final ResultSet resultSet;
+        private final MyList<WorkRecord> workRecords;
+        private final Constructor<WorkRecord> constructor;
+
+        private WorkRecordInstantiation(ResultSet resultSet) throws Exception {
+
+            this.resultSet = resultSet;
+            workRecords = new MyList<>();
+            constructor = WorkRecord.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            build();
+            constructor.setAccessible(false);
+            resultSet.close();
+        }
         @Override
         protected void build() throws Exception {
-
+            String ID = "id";
+            String PATIENT = "patient";
+            String CLINIC = "clinic";
+            String COMPLETE = "complete";
+            String ACCEPTED = "accepted";
+            String CLOSED = "closed";
+            String PHOTO = "photo";
+            String COMMENT = "comment";
+            while(resultSet.next()) {
+                WorkRecord workRecord = constructor.newInstance();
+                workRecord.setPatient(resultSet.getString(PATIENT));
+                workRecord.setClinic(resultSet.getString(CLINIC));
+                workRecord.setComplete(resultSet.getDate(COMPLETE).toLocalDate());
+                workRecord.setClosed(resultSet.getBoolean(CLOSED));
+                Blob photoBlob = resultSet.getBlob(PHOTO);
+                BufferedImage photo = ImageIO.read(photoBlob.getBinaryStream());
+                workRecord.setPhoto(photo);
+                workRecord.setComment(resultSet.getString(COMMENT));
+                setObjectPrivateField(workRecord, ID, resultSet.getInt(ID));
+                setObjectPrivateField(workRecord, ACCEPTED, resultSet.getDate(ACCEPTED).toLocalDate());
+                workRecords.add(workRecord);
+            }
         }
     }
 
