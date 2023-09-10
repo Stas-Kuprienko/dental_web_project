3b0763255597375c401bb741c5f92cd68a513707
Stanislav Kuprienko <stasi.architekk@gmail.com>
2023-09-10 13:46:24 +0600

    update InitializerDB.java

--- src/main/java/dental/database/InitializerDB.java
+++ src/main/java/dental/database/InitializerDB.java
@@ -17,45 +17,50 @@ public final class InitializerDB extends Thread {
 
     public final String ACCOUNT = """
             CREATE TABLE mydb.account (
-                id INT NOT NULL,
+                id INT NOT NULL AUTO_INCREMENT,
+                name VARCHAR(25),
                 login VARCHAR(15) NOT NULL,
+                password BLOB NOT NULL,
+                created DATE NOT NULL,
                 PRIMARY KEY (id)
-                );""";
+            );""";
 
     public final String WORK_RECORD = """
             CREATE TABLE mydb.work_record (
             	account_id INT NOT NULL,
-            	FOREIGN KEY(account_id) REFERENCES mydb.account(id) ON DELETE CASCADE,
-            	id INT NOT NULL,
+                FOREIGN KEY(account_id) REFERENCES mydb.account(id) ON DELETE CASCADE,
+                id INT NOT NULL,
             	patient VARCHAR(20),
-            	clinic VARCHAR(20),
-            	complete DATE,
+                clinic VARCHAR(20),
+                complete DATE,
             	accepted DATE NOT NULL,
-            	closed BOOLEAN,
-            	PRIMARY KEY (id)
-                );""";
+                closed BOOLEAN,
+                photo BLOB,
+                comment VARCHAR(45),
+                PRIMARY KEY (id)
+            );""";
 
     public final String PRODUCT = """
             CREATE TABLE mydb.product (
-            	work_id INT NOT NULL,
+                work_id INT NOT NULL,
                 title VARCHAR(15) NOT NULL,
                 quantity SMALLINT,
                 price INT NOT NULL,
                 FOREIGN KEY(work_id) REFERENCES mydb.work_record(id) ON DELETE CASCADE
-                );""";
+            );""";
 
     public final String REPORT = """
             CREATE TABLE mydb.report (
-            	r_year YEAR NOT NULL,
-            	r_month ENUM('january', 'february', 'march',
-            					'april', 'may', 'june', 'july',
-                                'august', 'september', 'october',
-                                'november', 'december') NOT NULL,
-            	r_id INT NOT NULL AUTO_INCREMENT,
-            	account_id INT NOT NULL,
+                r_year YEAR NOT NULL,
+                r_month ENUM('january', 'february', 'march',
+            				'april', 'may', 'june', 'july',
+                            'august', 'september', 'october',
+                            'november', 'december') NOT NULL,
+                r_id INT NOT NULL AUTO_INCREMENT,
+                account_id INT NOT NULL,
                 FOREIGN KEY(account_id) REFERENCES mydb.account(id)  ON DELETE CASCADE,
-            	PRIMARY KEY (r_id, account_id)
-            	);""";
+                PRIMARY KEY (r_id, account_id)
+            );""";
 
 
     @Override
