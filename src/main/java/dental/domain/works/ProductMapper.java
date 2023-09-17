c8d4a8f7f6e4f96a8aaf8bfd6e691503ad8b165b
Stanislav Kuprienko <stasi.architekk@gmail.com>
2023-09-17 15:44:08 +0600

    create WorkRecordDAO#instantiation

--- src/main/java/dental/domain/works/ProductMapper.java
+++ src/main/java/dental/domain/works/ProductMapper.java
@@ -1,5 +1,9 @@
 package dental.domain.works;
 
+import dental.domain.MyList;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
 public class ProductMapper {
@@ -8,33 +12,33 @@ public class ProductMapper {
 
     private int size;
 
-    private Node[] table;
+    private TypeNode[] table;
 
 
     public ProductMapper() {
-        table = new Node[DEFAULT_CAPACITY];
+        table = new TypeNode[DEFAULT_CAPACITY];
         size = 0;
     }
 
-    public boolean put(String type, int price) {
-        if (type == null) {
+    public boolean put(String title, int price) {
+        if (title == null) {
             throw new NullPointerException("key is empty");
         }
         if (size == table.length) {
             grow();
         }
-        int i = find(type);
+        int i = find(title);
         if (i > -1) {
             table[i].price = price;
         } else {
-            table[size] = new Node(type.toLowerCase(), price);
+            table[size] = new TypeNode(title.toLowerCase(), price);
             size++;
         }
         return true;
     }
 
-    public int getPrice(String type) {
-        int i = find(type);
+    public int getPrice(String title) {
+        int i = find(title);
         if (i == -1) {
             throw new NullPointerException("value is not found");
         } else {
@@ -42,8 +46,8 @@ public class ProductMapper {
         }
     }
 
-    public boolean removeType(String type) {
-        int i = find(type);
+    public boolean removeType(String title) {
+        int i = find(title);
         if (i == -1) {
             return false;
         } else {
@@ -56,28 +60,39 @@ public class ProductMapper {
         }
     }
 
-    public Product buildProduct(String type, int quantity) {
-        int i = find(type);
+    public Product buildProduct(String title, int quantity) {
+        int i = find(title);
         if (i == -1) {
-            throw new NoSuchElementException("type is not found");
+            throw new NoSuchElementException("type title is not found");
         } else {
-            Node node = table[i];
-            return new Product(node.type, (byte) quantity, node.price);
+            TypeNode node = table[i];
+            return new Product(node.title, (byte) quantity, node.price);
         }
     }
 
+    public MyList<Product> instantiateFromDB(ResultSet resultSet) {
+        MyList<Product> list = new MyList<>();
+        for (TypeNode t : table) {
+            try {
+                int q = resultSet.getInt(t.title);
+                if (q != 0) {
+                    list.add(buildProduct(t.title, q));
+                }
+            } catch (SQLException ignored) {}
+        }
+        return list;
+    }
 
-
-    private int find(String type) {
-        if (type == null) {
+    private int find(String title) {
+        if (title == null) {
             throw new NullPointerException("param is null");
         }
         if (size < 1) {
             return -1;
         }
-        type = type.toLowerCase();
+        title = title.toLowerCase();
         for (int i = 0; i < size; i++) {
-            if (table[i].type.equals(type)) {
+            if (table[i].title.equals(title)) {
                 return i;
             }
         }
@@ -85,16 +100,16 @@ public class ProductMapper {
     }
 
     private void grow() {
-        Node[] result = new Node[size << 1];
+        TypeNode[] result = new TypeNode[size << 1];
         System.arraycopy(table, 0, result, 0, table.length);
         this.table = result;
     }
-    private static final class Node {
-        final String type;
+    private static final class TypeNode {
+        final String title;
         int price;
 
-        Node(String type, int price) {
-            this.type = type;
+        TypeNode(String title, int price) {
+            this.title = title;
             this.price = price;
         }
     }
