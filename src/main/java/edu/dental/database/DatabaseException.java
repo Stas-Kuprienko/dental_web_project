0c0dfb912aa37c66e30f43a1e952f85f5a04ae01
Stanislav Kuprienko <stasi.architekk@gmail.com>
2023-10-05 02:22:30 +0600

    update DatabaseException.java

--- src/main/java/edu/dental/database/DatabaseException.java
+++ src/main/java/edu/dental/database/DatabaseException.java
@@ -2,6 +2,10 @@ package edu.dental.database;
 
 public class DatabaseException extends Exception {
 
+    public DatabaseException(String message) {
+        super(message);
+    }
+
     public DatabaseException(String message, Throwable cause) {
         super(message, cause);
     }
