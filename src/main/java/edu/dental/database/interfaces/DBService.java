9c91c7a3598ff05c4a56281c39783a4f5d537f85
Stanislav Kuprienko <stasi.architekk@gmail.com>
2023-10-07 23:20:46 +0600

    update MyDBService.java

--- src/main/java/edu/dental/database/interfaces/DBService.java
+++ src/main/java/edu/dental/database/interfaces/DBService.java
@@ -1,16 +1,17 @@
 package edu.dental.database.interfaces;
 
+import edu.dental.database.DatabaseException;
 import edu.dental.domain.entities.User;
 
 public interface DBService {
 
-    User authenticate(String login, String password);
+    User authenticate(String login, String password) throws DatabaseException;
 
     DAO<?> getUserDAO();
 
-    DAO<?> getWorkRecordDAO();
+    DAO<?> getWorkRecordDAO(Object... args) throws DatabaseException;
 
-    DAO<?> getProductDAO();
+    DAO<?> getProductDAO(Object... args) throws DatabaseException;
 
     TablesCreator getTablesCreator();
 
--- src/main/java/edu/dental/database/mysql_api/MyDBService.java
+++ src/main/java/edu/dental/database/mysql_api/MyDBService.java
@@ -1,8 +1,12 @@
 package edu.dental.database.mysql_api;
 
+import edu.dental.database.DatabaseException;
+import edu.dental.database.interfaces.DAO;
 import edu.dental.database.interfaces.DBService;
 import edu.dental.database.interfaces.TablesCreator;
-import edu.dental.database.interfaces.DAO;
+import edu.dental.database.mysql_api.dao.ProductMySql;
+import edu.dental.database.mysql_api.dao.UserMySql;
+import edu.dental.database.mysql_api.dao.WorkRecordMySql;
 import edu.dental.domain.entities.User;
 
 public class MyDBService implements DBService {
@@ -14,29 +18,36 @@ public class MyDBService implements DBService {
     private static final MyDBService instance;
 
 
-    public User authenticate(String login, String password) {
-        //TODO
-        return null;
+    public User authenticate(String login, String password) throws DatabaseException {
+        return new UserMySql().search(login, password).get(0);
     }
 
     @Override
     public DAO<?> getUserDAO() {
-        return null;
+        return new UserMySql();
     }
 
     @Override
-    public DAO<?> getWorkRecordDAO() {
-        return null;
+    public DAO<?> getWorkRecordDAO(Object... args) throws DatabaseException {
+        try {
+            return new WorkRecordMySql((User) args[0]);
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException | ClassCastException e) {
+            throw new DatabaseException(e.getMessage(), e.getCause());
+        }
     }
 
     @Override
-    public DAO<?> getProductDAO() {
-        return null;
+    public DAO<?> getProductDAO(Object... args) throws DatabaseException {
+        try {
+            return new ProductMySql((Integer) args[0], (Integer) args[1]);
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException | ClassCastException e) {
+            throw new DatabaseException(e.getMessage(), e.getCause());
+        }
     }
 
     @Override
-    public TablesCreator getTablesCreator() {
-        return null;
+    public synchronized TablesCreator getTablesCreator() {
+        return MySqlTablesCreator.instance;
     }
 
     public static synchronized DBService getInstance() {
