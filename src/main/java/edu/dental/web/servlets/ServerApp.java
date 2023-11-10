a83ac979e0f55dd7b0c0af539b08ac2f8eafe830
Stanislav Kuprienko <stasi.architekk@gmail.com>
2023-11-11 02:29:37 +0600

    something work with servlets

--- src/main/java/edu/dental/web/servlets/ServerApp.java
+++ src/main/java/edu/dental/web/servlets/ServerApp.java
@@ -1,4 +1,4 @@
-package edu.dental.server.application;
+package edu.dental.web.servlets;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -20,13 +20,19 @@ public class ServerApp extends HttpServlet {
     private static final String enter = """
             <!DOCTYPE html>
             <html>
+            <style>
+            body {
+              background-color: dimGrey;
+            }
+            body {color:white;}
+            </style>
             <body>
             <h2>Input your email and password please:</h2>
-            <form action="/dental/welcome">
+            <form action="/dental/welcome" method="post">
               <label for="email">email:</label><br>
               <input type="text" id="email" name="email" value=""><br>
               <label for="password">password:</label><br>
-              <input type="text" id="password" name="password" value=""><br><br>
+              <input type="password" id="password" name="password" value=""><br><br>
               <input type="submit" value="enter">
             </form>
             </body>
--- src/main/java/edu/dental/web/servlets/ServerWelcome.java
+++ src/main/java/edu/dental/web/servlets/ServerWelcome.java
@@ -1,4 +1,4 @@
-package edu.dental.server.application;
+package edu.dental.web.servlets;
 
 import edu.dental.domain.authentication.AuthenticationException;
 import edu.dental.domain.authentication.Authenticator;
@@ -16,7 +16,7 @@ public class ServerWelcome extends HttpServlet {
     User user;
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter writer = response.getWriter();
 
         String login = request.getParameter("email");
@@ -24,6 +24,8 @@ public class ServerWelcome extends HttpServlet {
 
         try {
             user = Authenticator.authenticate(login, password);
+            request.getSession().setAttribute("user", user);
+
         } catch (AuthenticationException e) {
             writer.write("no such user.");
             request.getRequestDispatcher("dental/").forward(request, response);
@@ -34,19 +36,27 @@ public class ServerWelcome extends HttpServlet {
     private static final String actions = """
             <!DOCTYPE html>
             <html>
+            <style>
+                body {
+                    background-color: dimGrey;
+                }
+                body {color:white;}
+            </style>
             <body>
             <h1>Welcome, %s!</h1>
             <h2>Dental mechanic service</h2>
-            <form action="/dental/new-product-type">
-              <input type="submit" value="add new product type">
+            <form action="/dental/new-product-type" method="post">
+                <input type="submit" value="add new product type">
+            </form>
             <form action="/dental/new-work">
-              <input type="submit" value="add new work">
+                <input type="submit" value="add new work">
+            </form>
             <form action="/dental/open-work-list">
-              <input type="submit" value="open work list">
+                <input type="submit" value="open work list">
+            </form>
             <form action="/dental/save-report-file">
-              <input type="submit" value="save report to file">
+                <input type="submit" value="save report to file">
             </form>
             </body>
-            </html>
-            """;
+            </html>""";
 }
